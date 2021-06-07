faf98b42343aaab6f0a19fb27e9cd1520709fa6b
Ehsan Nasiri <ehsannas@gmail.com>
2021-06-07 16:39:18 -0500

    Build and Serve React App for 'home-client'.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,12 @@ const path = require("path");
 
 const app = express();
 app.use(express.static(path.join(__dirname, "..", "admin-client", "build")));
+app.use(express.static(path.join(__dirname, "..", "home-client", "build")));
 
 app.get("/home", (request: any, response: any) => {
-  functions.logger.info("Hello from /home!", { structuredData: true });
-  response.send("Hello from /home!");
+  response.sendFile(
+    path.join(__dirname, "..", "home-client", "build", "index.html")
+  );
 });
 
 app.get("/admin", (request: any, response: any) => {
