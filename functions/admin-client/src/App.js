2e8252ecb5c54f0ed4adad4beaecc7e6b1055751
Iman Rahmatizadeh <678917+i14h@users.noreply.github.com>
2021-06-08 08:13:28 -0700

    Add an authenticated endpoint that does work /admin/work (#4)
    * resolve merge conflict
    add auth support
    
    * Cleaning up commit.
    
    * Adding a network to the admin/work endpoint in the admin client
    
    Co-authored-by: Iman Rahmati <imanrahmati@google.com>
    Co-authored-by: Rafi Khan <khanrafi@google.com>

--- functions/admin-client/src/App.js
+++ functions/admin-client/src/App.js
@@ -1,12 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('Loading...')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('admin/work');
+
+        setData(result.status + ' ' + result.data);
+      } catch(e) {
+        if(e.isAxiosError) {
+          const result = e.response;
+
+        setData(result.status + ' ' + result.data);
+        } else {
+          setData(e.message);
+        }
+      }
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is the Admin WebApp.</p>
+
+        <p>POST admin/work response -- {data}</p>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,62 @@
 import * as functions from "firebase-functions";
-const express = require("express");
-const path = require("path");
+import * as ex from 'express';
 
+const path = require("path");
+const admin = require('firebase-admin');
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({origin: true});
+
+admin.initializeApp();
 const app = express();
 app.use(express.static(path.join(__dirname, "..", "admin-client", "build")));
 app.use(express.static(path.join(__dirname, "..", "home-client", "build")));
+
+const validateFirebaseIdToken = async (req: ex.Request, res: ex.Response, next: any) => {
+  functions.logger.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+      !(req.cookies && req.cookies.__session)) {
+    functions.logger.error(
+        'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.'
+    );
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else if(req.cookies) {
+    functions.logger.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    return;
+  } catch (error) {
+    functions.logger.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
+
+app.use(cors);
+app.use(cookieParser);
+
 
 app.get("/home", (request: any, response: any) => {
   response.sendFile(
@@ -17,6 +69,8 @@ app.get("/admin", (request: any, response: any) => {
     path.join(__dirname, "..", "admin-client", "build", "index.html")
   );
 });
+
+app.get("/admin/work", validateFirebaseIdToken);
 
 app.get("/", (request: any, response: any) => {
   response.status(200).send();
